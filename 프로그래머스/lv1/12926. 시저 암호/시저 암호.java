b8bea46be9d113f4fcb6c61b4bf2375ed3de7b8f
leesco22 <127731995+leesco22@users.noreply.github.com>
2023-04-11 16:28:14 +0900

    [level 1] Title: 시저 암호, Time: 2.56 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isLowerCase(ch)){ //a
+                sb.append((char)((ch-'a'+n)%26 + 'a'));
+            }else if(Character.isUpperCase(ch)){ //A
+                sb.append((char)((ch-'A'+n)%26 + 'A'));
+            }else{
+                sb.append(' ');
+            }
+        }
+        return sb.toString();
+    }
+}
